647202e4ced844ea8e0d66494fc9867437b4d873
VitTrang <hanphi12022003@gmail.com>
2024-11-21 22:49:39 +0700

    yêu cầu 5

--- wp-content/themes/jobscout/functions.php
+++ wp-content/themes/jobscout/functions.php
@@ -111,3 +111,81 @@ function enqueue_SeparateBS51() {
     wp_enqueue_script('enqueue_SeparateBS51', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js');
 }
 add_action('wp_enqueue_scripts', 'enqueue_SeparateBS51');
+function jobscout_display_blog_section() {
+    $blog_heading = get_theme_mod( 'blog_section_title', __( 'NEWEST BLOG ENTRIES', 'jobscout' ) );
+    $sub_title    = get_theme_mod( 'blog_section_subtitle', __( '', 'jobscout' ) );
+    $blog         = get_option( 'page_for_posts' );
+    $label        = get_theme_mod( 'blog_view_all', __( 'See More Posts', 'jobscout' ) );
+    $hide_author  = get_theme_mod( 'ed_post_author', false );
+    $hide_date    = get_theme_mod( 'ed_post_date', false );
+    $ed_blog      = get_theme_mod( 'ed_blog', true );
+
+    $args = array(
+        'post_type'           => 'post',
+        'post_status'         => 'publish',
+        'posts_per_page'      => 6,
+        'ignore_sticky_posts' => true
+    );
+
+    $qry = new WP_Query( $args );
+
+    if( $ed_blog && ( $blog_heading || $sub_title || $qry->have_posts() ) ) {
+        ?>
+        <div id="newest-blog">
+            <section id="blog-section" class="article-section">
+                <div class="container">
+                    <?php 
+                    if( $blog_heading ) echo '<h2 class="section-title">' . esc_html( $blog_heading ) . '</h2>';
+                    if( $sub_title ) echo '<div class="section-desc">' . wpautop( wp_kses_post( $sub_title ) ); 
+                    ?>
+                    <?php if( $qry->have_posts() ){ ?>
+                        <div class="row blog-all-item gy-5">
+                            <?php 
+                            while( $qry->have_posts() ) {
+                                $qry->the_post(); ?>
+                                <div class="col-md-6 blog-item">
+                                    <div class="p-3 bg-light blog-item1">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <figure class="post-thumbnail">
+                                                    <a href="<?php the_permalink(); ?>" class="post-thumbnail">
+                                                    <?php 
+                                                        if( has_post_thumbnail() ){
+                                                            the_post_thumbnail( 'jobscout-blog', array( 'itemprop' => 'image' ) );
+                                                        }else{ 
+                                                            jobscout_fallback_svg_image( 'jobscout-blog' ); 
+                                                        }                            
+                                                    ?>                        
+                                                    </a>
+                                                </figure>
+                                            </div>
+                                            <div class="col-md-7 blog-item-text">
+                                                <article class="post">
+                                                    <header class="entry-header">
+<h3 class="entry-title">
+                                                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                                        </h3>
+                                                        <div class="entry-excerpt">
+                                                            <?php the_excerpt(); ?>
+                                                        </div>
+                                                        <div class="read-more">
+                                                            <a href="<?php the_permalink(); ?>">Read More</a>
+                                                        </div>
+                                                    </header>
+                                                </article>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>			
+                                <?php 
+                            }
+                            wp_reset_postdata();
+                            ?>
+                        </div>
+                    <?php } ?>
+                </div>
+            </section>
+        </div>
+        <?php 
+    }
+}
--- wp-content/themes/jobscout/index.php
+++ wp-content/themes/jobscout/index.php
@@ -14,39 +14,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		
+    <div id="primary" class="content-area">
+        
         <?php 
         /**
          * Before Posts hook
         */
         do_action( 'jobscout_before_posts_content' );
+
+        // Gọi hàm hiển thị blog section
+        if ( function_exists( 'jobscout_display_blog_section' ) ) {
+            jobscout_display_blog_section();
+        }
         ?>
         
         <main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
+       
+        </main><!-- #main -->
         
         <?php
         /**
@@ -56,7 +41,7 @@ get_header(); ?>
         do_action( 'jobscout_after_posts_content' );
         ?>
         
-	</div><!-- #primary -->
+    </div><!-- #primary -->
 
 <?php
 get_sidebar();
